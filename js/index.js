84d2db23ba474267dd200610a20e754339923ff4
adrianbparra <adrianplane12@gmail.com>
2019-11-02 11:55:33 -0700

    added event listners for load, mouseover, scroll, dblclick

--- js/index.js
+++ js/index.js
@@ -1,1 +1,67 @@
 // Your code goes here
+let logoHeading = document.querySelector(".logo-heading");
+let navLinks = document.querySelectorAll(".nav-link");
+let introImg = document.querySelector(".intro img");
+const section = document.querySelectorAll("section");
+const headerh1 = document.querySelector(".intro h2");
+const buttons = document.querySelectorAll(".btn");
+const sectionImgs = document.querySelectorAll(".img-content");
+const destImg = document.querySelector(".content-destination img");
+
+
+// console.log(headerh1);
+
+
+
+
+window.addEventListener("load", () =>{
+    logoHeading.textContent = "Sub Nuf";
+});
+
+
+
+introImg.addEventListener("mouseover", () =>{
+    introImg.removeAttribute("style")
+    introImg.style.transform = "rotate(180deg)";
+});
+
+introImg.addEventListener("mouseout", () =>{
+    introImg.style.transform = "rotate(0deg)";
+});
+
+
+let colors = ["blue","red", "orange", "green", "yellow"];
+
+window.addEventListener("scroll",(e) => {
+    // console.log(window.scrollY)
+
+    let posSet = Math.ceil(window.scrollY/200);
+    let ran = Math.floor(Math.random()*5);
+    console.log(ran);
+    // console.log(posSet);
+    section.forEach(sec => {
+        sec.style.backgroundColor = colors[ran];
+    })
+});
+
+
+document.addEventListener("keydown", (e)=>{
+    headerh1.style.textAlign = "right";
+    console.log(e)
+});
+
+
+
+
+
+sectionImgs.forEach(sec => {
+    // console.log(sec);
+    sec.addEventListener("dblclick", ()=> {
+        sec.classList.toggle("hide")
+    });
+
+});
+
+
+console.log(destImg);
+
